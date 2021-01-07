68d7571ae11b57cc0a3d39d1b55dfe7ad3fc5ec0
Zia Fadhilah <ziafadhilah98@gmail.com>
2021-01-07 16:14:42 +0700

    update database

--- kayu_online.sql
+++ kayu_online.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 18, 2020 at 05:03 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.10
+-- Host: localhost:3306
+-- Generation Time: Jan 07, 2021 at 03:12 AM
+-- Server version: 5.6.39-83.1
+-- PHP Version: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `kayu_online`
+-- Database: `kayuoine_id`
 --
 
 -- --------------------------------------------------------
@@ -52,9 +53,9 @@ INSERT INTO `fb_users` (`id`, `oauth_provider`, `oauth_uid`, `first_name`, `last
 (4, 'google', '111692835073024361290', 'Andi Digital', 'Andi Digital', 'andigital@qmail.id', NULL, 'https://lh6.googleusercontent.com/-9hbgOuaum08/AAAAAAAAAAI/AAAAAAAAAAA/AMZuuclOaNn2SM9o8YZLlA294o73AovYvw/s96-c/photo.jpg', NULL, '2020-10-31 09:09:44', '2020-10-31 09:09:44'),
 (5, 'facebook', '349702236322807', 'MI', 'Unpi', 'informatika.unpi@gmail.co', NULL, 'https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=349702236322807&width=300&ext=1606876529&hash=AeQQFOmMEsujkb88mjo', NULL, '2020-11-02 09:35:29', '2020-11-02 09:35:29'),
 (6, 'facebook', '105062754744457', 'Hirakarya', 'Abadi', 'kayuonline2020@gmail.com', NULL, 'https://scontent-iad3-1.xx.fbcdn.net/v/t1.30497-1/p200x200/84628273_176159830277856_972693363922829312_n.jpg?_nc_cat=1&ccb=2&_nc_sid=12b3be&_nc_ohc=HuadhmtccTIAX-tH7bh&_nc_ht=scontent-iad3-1.xx&tp=6&o', NULL, '2020-11-10 10:17:19', '2020-11-10 10:17:19'),
-(7, 'google', '114711351661231901247', 'zia fadhilah', 'zia fadhilah', 'ziafadhilah2112@gmail.com', NULL, 'https://lh5.googleusercontent.com/-PBKLdZBamiE/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucndVAzxsWvR-IvtsILw7_c55ipnWA/s96-c/photo.jpg', NULL, '2020-11-13 03:44:27', '2020-11-13 03:44:27'),
 (8, 'facebook', '3318654994856714', 'Armansyah', 'Hakim', 'kirigakure_hime@yahoo.com', NULL, 'https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=3318654994856714&width=300&ext=1607678373&hash=AeQFuoLTj71eaZ2g-Wo', NULL, '2020-11-11 04:19:33', '2020-11-11 04:19:33'),
-(9, 'google', '107171355917691006199', 'Armansyah Hakim', 'Armansyah Hakim', 'armansyahhakim263@gmail.c', NULL, 'https://lh3.googleusercontent.com/a-/AOh14Gjm1769hLV6jjMTwyiZyMYY9U87azWubuz-B-uasQ=s96-c', NULL, '2020-11-18 03:14:32', '2020-11-18 03:14:32');
+(9, 'google', '107171355917691006199', 'Armansyah Hakim', 'Armansyah Hakim', 'armansyahhakim263@gmail.c', NULL, 'https://lh3.googleusercontent.com/a-/AOh14Gjm1769hLV6jjMTwyiZyMYY9U87azWubuz-B-uasQ=s96-c', NULL, '2020-11-18 03:14:32', '2020-11-18 03:14:32'),
+(11, 'google', '108293010694596594930', 'Dedy Kurnia', 'Dedy Kurnia', 'dedykurnia@gmail.com', NULL, 'https://lh3.googleusercontent.com/a-/AOh14GiFKX2vg3Itl4xrhycqxxJxMr_UwVXEzwf3jpKlCA=s96-c', NULL, '2020-12-15 07:59:58', '2020-12-15 07:59:58');
 
 -- --------------------------------------------------------
 
@@ -84,7 +85,7 @@ INSERT INTO `notifikasi` (`id`, `admin_reg`, `stok_barang`, `bukti_transfer`) VA
 
 CREATE TABLE `orders` (
   `id` bigint(20) NOT NULL,
-  `id_pemesan` text DEFAULT NULL,
+  `id_pemesan` text,
   `sku_product` char(20) DEFAULT NULL,
   `tanggal` datetime DEFAULT NULL,
   `nama_product` varchar(255) DEFAULT NULL,
@@ -98,6 +99,13 @@ CREATE TABLE `orders` (
   `product_size_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `id_pemesan`, `sku_product`, `tanggal`, `nama_product`, `ukuran`, `gambar_product`, `id_product`, `jumlah`, `harga`, `total`, `status`, `product_size_id`) VALUES
+(107, '103', 'KO JT01', '2020-12-15 11:03:01', 'Jati', ' 1 meter', 'assets/img/default.png', 3, 1, 120000, 120000, 'done', 5);
+
 -- --------------------------------------------------------
 
 --
@@ -110,7 +118,7 @@ CREATE TABLE `products` (
   `product_code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `detail` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `detail` text COLLATE utf8mb4_unicode_ci,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -177,18 +185,17 @@ CREATE TABLE `product_has_sizes` (
 --
 
 INSERT INTO `product_has_sizes` (`id`, `product_id`, `product_size_id`, `price`, `stock`, `created_at`, `updated_at`, `sold`) VALUES
-(1, 1, 1, '10000', 100, NULL, NULL, 1),
-(2, 1, 2, '20000', 100, NULL, NULL, 2),
-(4, 2, 1, '85000', 100, NULL, NULL, 0),
-(5, 3, 1, '120000', 100, NULL, NULL, 3),
-(6, 3, 5, '150000', 100, NULL, NULL, 2),
-(7, 2, 1, '100000', 100, NULL, NULL, 2),
-(9, 4, 1, '20000', 100, NULL, NULL, NULL),
-(10, 5, 6, '123000', 100, NULL, NULL, NULL),
-(13, 2, 9, '20000', 100, NULL, NULL, NULL),
-(16, 11, 2, '8000000', 100, NULL, NULL, NULL),
-(19, 14, 1, '20000', 100, NULL, NULL, NULL),
-(27, 17, 1, '10', 8, NULL, NULL, NULL);
+(1, 1, 1, 10000, 100, NULL, NULL, 1),
+(2, 1, 2, 20000, 100, NULL, NULL, 2),
+(4, 2, 1, 85000, 100, NULL, NULL, 0),
+(5, 3, 1, 120000, 100, NULL, NULL, 3),
+(6, 3, 5, 150000, 99, NULL, NULL, 2),
+(7, 2, 1, 100000, 100, NULL, NULL, 2),
+(9, 4, 1, 20000, 100, NULL, NULL, NULL),
+(10, 5, 6, 123000, 100, NULL, NULL, NULL),
+(13, 2, 9, 20000, 100, NULL, NULL, NULL),
+(16, 11, 2, 8000000, 100, NULL, NULL, NULL),
+(19, 14, 1, 20000, 100, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -200,7 +207,7 @@ CREATE TABLE `product_images` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `product_id` bigint(20) UNSIGNED NOT NULL,
   `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `first` tinyint(1) NOT NULL DEFAULT 0,
+  `first` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -216,8 +223,7 @@ INSERT INTO `product_images` (`id`, `product_id`, `image`, `first`, `created_at`
 (6, 4, 'assets/img/product/eksotik mg 1.png', 0, NULL, NULL),
 (7, 5, 'assets/img/product/eksotik br 1.png', 0, NULL, NULL),
 (12, 11, 'assets/img/product/Eksotik-GSL.jpeg', 0, NULL, NULL),
-(15, 14, 'assets/img/product/nyatoh.png', 0, NULL, NULL),
-(23, 17, 'assets/img/product/43694584_2306085279432891_8956910796932120576_o.jpg', 0, NULL, NULL);
+(15, 14, 'assets/img/product/nyatoh.png', 0, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -285,20 +291,31 @@ INSERT INTO `product_thumbnail` (`id`, `product_id`, `kode_product`, `kode_kayu`
 
 CREATE TABLE `reservasi` (
   `id` bigint(20) NOT NULL,
-  `id_pemesan` text DEFAULT NULL,
+  `id_pemesan` text,
   `nama_pemesan` varchar(255) DEFAULT NULL,
   `alamat_pemesan` varchar(255) DEFAULT NULL,
   `kode_pos` char(50) DEFAULT NULL,
   `kota` varchar(255) DEFAULT NULL,
   `kontak` char(50) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `note` text DEFAULT NULL,
+  `note` text,
   `bukti_transaksi` char(100) DEFAULT NULL,
   `status` varchar(100) DEFAULT NULL,
   `bank` varchar(100) DEFAULT NULL,
-  `verifikasi` text DEFAULT NULL,
+  `verifikasi` text,
   `tanggal` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reservasi`
+--
+
+INSERT INTO `reservasi` (`id`, `id_pemesan`, `nama_pemesan`, `alamat_pemesan`, `kode_pos`, `kota`, `kontak`, `email`, `note`, `bukti_transaksi`, `status`, `bank`, `verifikasi`, `tanggal`) VALUES
+(186, '103', 'ziafadhilah', 'coba', '1', 'coba', '081212151324', 'ziafadhilah98@gmail.com', ' ', 'assets/img/bukti_tf/DSC02171.JPG', 'done', 'Mandiri', NULL, '2020-12-15 11:03:32'),
+(277, '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'proses_3', NULL, NULL, NULL),
+(278, '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'proses_3', NULL, NULL, NULL),
+(279, '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'proses_3', NULL, NULL, NULL),
+(280, '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'proses_3', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -309,7 +326,7 @@ CREATE TABLE `reservasi` (
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `nama` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
+  `email` varchar(80) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `no_hp` char(50) DEFAULT NULL,
@@ -322,12 +339,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nama`, `email`, `username`, `password`, `no_hp`, `role`, `status`) VALUES
-(29, 'Armansyah', 'armansyah.hakim@gmail.com', 'Arman', 'KzhuWEF1MUZBMnBCRjZiR0R4K0J3QT09', '085974785490', 0x75736572, NULL),
 (31, 'Admin', 'kayuonline.id@gmail.com', 'leaq6vp2yph3', 'c2tnWjg0UUZxakROamNyVjRZMDdpdz09', '081241632727', 0x61646d696e, 'active'),
-(34, 'Andi Digital', 'soni.mkom@gmail.com', 'andidigital', 'VzNkd2svcGVVOTNPMk52N2VrOHhMUT09', '081222902400', 0x61646d696e, 'active'),
-(40, 'Angel', 'Arhaisme2001@gmail.com', 'Angel', 'WDB0c3pSS00wZmgwdzhSQTc2ZWpGZz09', '085974785490', 0x75736572, NULL),
-(43, 'Bayu', 'kirigakure_hime@yahoo.com', 'Bayu31', 'MkpmcHg0VG9CU0NaTllPUmRDNktKQT09', '085974785490', 0x75736572, NULL),
-(46, 'ziafadhilah', 'ziafadhilah98@gmail.com', 'ziafadhilah', 'aXZpNmhRT0xaL1hpOWNWYStWRlkwdz09', '081212151324', 0x75736572, NULL);
+(113, 'Andi Lukman ', 'andilukmanx@gmail.com', 'andipsycho', 'WXM4SzdtcTRFSXI4Z3JlaW1mcW9DZz09', '07272682', 0x75736572, NULL),
+(155, NULL, NULL, NULL, 'V3dVaGhmdFUrREY0ajBRK3pNbFNNdz09', NULL, 0x75736572, NULL),
+(156, NULL, NULL, NULL, 'V3dVaGhmdFUrREY0ajBRK3pNbFNNdz09', NULL, 0x75736572, NULL);
 
 -- --------------------------------------------------------
 
@@ -347,7 +362,18 @@ CREATE TABLE `user_token` (
 --
 
 INSERT INTO `user_token` (`id`, `email`, `token`, `date_created`) VALUES
-(27, 'kayuonline2020@gmail.com', '47eExNHDHrjZyMbbKBc4ckYnQRKEwtuXf8tIHi2iyWs=', '2020-10-31');
+(63, 'soni.mkom@gmail.com', '6x6lxdU3szrqpKh5HB74cgNivKRcOO8RpwUoxCn6DTQ=', '2020-12-19'),
+(64, 'soni.mkom@gmail.com', 'CR7DMn8S+77UkZ1MCb+K0bBOgyDisLp+QRaGhbBYhuw=', '2020-12-19'),
+(65, 'soni.mkom@gmail.com', 'kBH2Ex8eHa4gV4Wz3HKHY+TXIfk1+v6B+ZRqAzQJa8g=', '2020-12-19'),
+(66, 'soni.mkom@gmail.com', '09pMepmkjmRgQ31xuVDnfNBFj7ZrBE2PlMxgS4pekGU=', '2020-12-19'),
+(67, 'soni.mkom@gmail.com', 'wIbGnzbaWF9KYOkOoJd9h6Rn6g3u6sbmY5qzJWswsyU=', '2020-12-19'),
+(68, 'andilukmanx@gmail.com', 'zPl3hrXDV8O8N06MTOb1lLFbY0tast9Wig3fuYvUZRE=', '2020-12-19'),
+(75, 'andilukmanx@gmail.com', 'wHJ/b+1pgQUF4K9EQP8QhpV4KOtrgIX7f0Fj3KOk5Ho=', '2020-12-21'),
+(76, 'andilukmanx@gmail.com', 'lrhj5qmZTw5+ViZnclwuOyZsFGNcKRwJB+SC601RBiI=', '2020-12-22'),
+(77, 'soni.mkom@gmail.com', 'WeBiDazSVSI+PVfJEHnxqAGCrRSRGfz74IBNy2m4bEI=', '2020-12-22'),
+(78, 'soni.mkom@gmail.com', 'f9gD0iy+0mKaxoPD/zw6xDJlyBy6WwW/zHMc7MRRubY=', '2020-12-22'),
+(79, 'soni.mkom@gmail.com', '9jdKOnEHrI8+GKd8QIvha+zpDJxiElnrSsLoV0SpVPM=', '2020-12-22'),
+(80, 'soni.mkom@gmail.com', 'rerndZW3Xke4gAR+avo6ml5Y8C2nijqvA/5C5cvEoCQ=', '2020-12-22');
 
 --
 -- Indexes for dumped tables
@@ -417,7 +443,8 @@ ALTER TABLE `reservasi`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `user_token`
@@ -433,7 +460,7 @@ ALTER TABLE `user_token`
 -- AUTO_INCREMENT for table `fb_users`
 --
 ALTER TABLE `fb_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `notifikasi`
@@ -445,13 +472,13 @@ ALTER TABLE `notifikasi`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `product_categories`
@@ -463,13 +490,13 @@ ALTER TABLE `product_categories`
 -- AUTO_INCREMENT for table `product_has_sizes`
 --
 ALTER TABLE `product_has_sizes`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `product_images`
 --
 ALTER TABLE `product_images`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `product_sizes`
@@ -487,19 +514,19 @@ ALTER TABLE `product_thumbnail`
 -- AUTO_INCREMENT for table `reservasi`
 --
 ALTER TABLE `reservasi`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=281;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=157;
 
 --
 -- AUTO_INCREMENT for table `user_token`
 --
 ALTER TABLE `user_token`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=107;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
